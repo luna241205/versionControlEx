f9016023e1c6803d6f3a206979c0d762f14a885c
trytr <trytr@192.168.200.112>
2024-12-20 08:26:29 +0900

    dev C merge

--- VersionControlEx/src/vc/A.java
+++ VersionControlEx/src/vc/A.java
@@ -3,14 +3,11 @@ package vc;
 public class A {
 
 	public static void main(String[] args) {
-<<<<<<< HEAD
 		System.out.println("A");
 		System.out.println("master A correction");
 		System.out.println("devB branch");
-=======
 		System.out.println("A");
 		System.out.println("C correction");
->>>>>>> refs/heads/devC
 	}
 
 }
